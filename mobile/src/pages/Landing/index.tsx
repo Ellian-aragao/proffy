98b53bf084ea79b3040c6bb2ad53b4a98f27539e
ellian_aragao <ellian.aragao@gmail.com>
2020-08-11 23:05:02 -0300

    criação pagina giveClasses

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,31 +1,49 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import styles from './styles';
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClasses from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
+
+  function handleNavigateToStudyPage() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
-      
+
       <Text style={styles.title}>
         Seja bem-vindo, {'\n'}
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
       </Text>
-      
+
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          style={[styles.button, styles.buttonPrimary]} 
+          onPress={handleNavigateToStudyPage}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
+        </RectButton>
 
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <RectButton 
+          style={[styles.button, styles.buttonSecondary]}
+          onPress={handleNavigateToGiveClassesPage}
+        >
           <Image source={giveClasses} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
---
+++ mobile/src/pages/StudyTabs/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+
+
+function StudyTabs() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+export default StudyTabs;
